86b017c2912976e08e41f1fc937d396c31b2976d
Ashley <85172771+ashuuuriii@users.noreply.github.com>
2025-06-24 19:18:34 +0100

    Add server initialisation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"local/go-shortener/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Print("hello world")
+	r := chi.NewRouter()
+
+	r.Post("/shorten", handlers.Shorten)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		slog.Info("Server is starting...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v", err)
+		}
+	}()
+
+	<-sigChan
+
+	slog.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %v", err)
+	}
+
+	slog.Info("Server shutdown complete.")
 }
